6b5a67259ed21c00ad7335ea84247f59f4cfd0ab
{HashikawaTomomi} <{hashikawa.tomomi@itfllc.co.jp}>
2022-12-08 13:02:43 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
 import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+const [todoText, setTodoText] = useState(['']);
+
+const [todos, setTodos] = useState(['']);
+
+const [editTodos, setEditTodos] = useState([''])
+
+const onChangeTodos = (event) => {
+  setTodos(event.target.value);
+};
+
+const onClickAdd = () => {
+  if (todos === '') return;
+  const newTodos = [...todoText, todos];
+  setTodoText(newTodos);
+  setTodos('');
+};
+
+const onClickDelete = (index) => {
+  const newTodos = [...todoText];
+  newTodos.splice(index, 1);
+  setTodoText(newTodos);
+};
+
+const onClickEdit = (index) => {
+  const newTodoText = [...todoText];
+  newTodoText.splice(index, 1);
+
+};
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div>
+      <div>
+        <h1>TODOリスト</h1>
+        <select>
+          <option>全て表示</option>
+          <option>未着手</option>
+          <option>進行中</option>
+          <option>完了</option>
+        </select>
+      </div>
+      <ul>
+        {todoText.map((todo ,index) => {
+          return  (
+          <li key={todo}>
+          <p>{todo}</p>
+          <button onClick={() => onClickEdit(index)}>編集</button>
+          <button onClick={() => onClickDelete(index)}>削除</button>
+        </li>
+          );
+        })}
+      </ul>
     </div>
+    <div>
+      <p>追加フォーム</p>
+      <input type='text' label='タイトル' value={todos} onChange={onChangeTodos} />
+      <button onClick={onClickAdd}>作成</button>
+    </div>
+    <div>
+      <p>編集フォーム</p>
+            <input type='text' label='新しいタイトル' value={todoText}/>
+      <button>編集を保存</button>
+      <button>キャンセル</button>
+    </div>
+    </>
   );
 }
 
